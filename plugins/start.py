afb3a50d32694f5ceb2d917111dfe16adf8ab81e
RexZeal <74536249+RexZeal@users.noreply.github.com>
2021-09-26 11:23:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/RexZeal")]
+            "Report Bugs ✅", url="https://t.me/RexZeal")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
