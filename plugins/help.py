3db0f00fa5272b201ebe2235435b16fab61f56fe
RexZeal <74536249+RexZeal@users.noreply.github.com>
2021-11-04 20:13:12 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,10 +3,8 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt1 = f"📌 No playlist support. Just send the YouTube Url"
+    helptxt1 = f"No playlist support. Just send the YouTube Url"
     helptxt2 = f"📌 Get the youtube url From @vid"
-    helptxt3 = f"📌 Contact @RexZeal if you have any problems"
 
     await message.reply_text(helptxt1)
     await message.reply_text(helptxt2)
-    await message.reply_text(helptxt3)
